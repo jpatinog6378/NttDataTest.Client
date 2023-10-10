c69037785583b171dcbcb6298d8417cbbf88c694
Sebastian Patiño <jpatinog6378@universidadean.edu.co>
2023-10-09 22:38:02 -0500

    first commit

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,29 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { LibraryHomeComponent } from './pages/library-home/library-home.component';
+import { AuthorPageComponent } from './pages/author-page/author-page.component';
+import { BookPageComponent } from './pages/book-page/book-page.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: 'library-home',
+    component: LibraryHomeComponent
+  },
+  {
+    path : 'author-page',
+    component : AuthorPageComponent
+  },
+  {
+    path : 'book-page',
+    component : BookPageComponent
+  },
+  {
+    path: '',
+    redirectTo: 'library-home',
+    pathMatch: 'full'
+  }
+
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
---
+++ src/app/pages/library-home/library-home.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-library-home',
+  templateUrl: './library-home.component.html',
+  styleUrls: ['./library-home.component.scss']
+})
+export class LibraryHomeComponent {
+
+}
